fa05b705b823dfb4981f7a27d776e4d9c7e63d3d
JoeDevwWeb <pjnorbi@gmail.com>
2022-06-13 09:27:29 -0300

    versao0.5

--- script/firebase.js
+++ script/firebase.js
@@ -10,63 +10,14 @@
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-  const auth = firebase.auth();
-  
-  
-  // Autenticação Cadrastar
-  
-  // firebase.auth().onAuthStateChanged((user) => {
-  //   if (user){
-  //      singUp.reset();
-       
-  //      window.location.href = "../index.html";
-       
-  //   }else{
-  //     alert(error);
-  //   }
-  // });
-  
-  const singUp = document.getElementById('formulario');
-  
-  singUp.onsubmit = (event) => {
-    event.preventDefault()
- 
-   const email = singUp.querySelector('[name=email]').value; 
-   const senha = singUp.querySelector('[name=password]').value;
 
-  firebase.auth().createUserWithEmailAndPassword(email, senha)
-    .then(() => {
-      alert("Usuario criado");
-    }).catch((error) => {
-      console.log(error);
-    });
-
-
- };
-
-
-
-//  Logar
-
-// const singIn = document.getElementById('formulario-Entrar');
-
-// singIn.onsubmit = (event) => {
-//   event.preventDefault()
- 
-//   const email = singIn.querySelector('[name=email]').value; 
-//   const senha = singIn.querySelector('[name=password]').value;
-
-//   firebase.auth().singInWithEmailAndPassword(email, senha)
-//     .then(() => {
-//       window.location.href = "../index.html";
-//     })
-//     .catch((error) => {
-//       if (error.code == 'auth/wrong-password' || error.code == "auth/user-not-found"){
-//         alert('E-mail ou senha inválidos');
-//         return;
-//       }
-//       alert('Nao foi possivel efetuar')
-//     })
-
-// };
+    
+  // Desconectar a conta
+function logout() {
+  firebase.auth().signOut()
+ .then(() => {
+   window.location.href = "./html/login.html";
+ }).catch(() => {
+   alert("Erro ao fazer logout");
+ });
+}
